98262eff4d9a7bc1a627464cabff999ddd323fba
Kamal Penmetcha <kpenmetc@asu.edu>
2020-10-06 13:21:51 +0530

    Second Version

--- 360Assignment/src/cse360assignment02/AddingMachine.java
+++ 360Assignment/src/cse360assignment02/AddingMachine.java
@@ -3,12 +3,14 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
   
   /**
    * creating a method AddingMachine and setting total = 0 and history = "0"
    */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
   /**
@@ -22,27 +24,35 @@ public class AddingMachine {
   
   /**
    * add method adds two values by taking an integer as a parameter
+   * update the history of operation
    */
   
   public void add (int value) {
+	  total = total + value;
+	  history = history + "+ " + value;
   }
 
   /**
    * subtract method subtracts two values by taking an integer as a parameter
+   * update the history of operations
    */
   public void subtract (int value) {
+	  total = total - value;
+	  history = history + "- " + value;
   }
 
   /**
-   * toString method returns the history of the actions taking place
+   * toString method returns the history of the operations taking place
    */
   public String toString () {
-    return "";
+	  return history;
   }
 
   /**
    * clear method clears the memory 
    */
   public void clear() {
+	  total = 0;
+	  history = "0";
   }
 }
